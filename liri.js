d5660d213dbec6b7ea5b09f9e651a6e4d1143deb
susehan <susannahan27@gmail.com>
2018-07-24 17:09:23 -0400

    updated files

--- liri.js
+++ liri.js
@@ -14,6 +14,9 @@ var Twitter = require('twitter');
 function findMovie(title) {
     var URL = "https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
     request(URL, function(err, response, body) {
+        if (err) {
+            return console.log("Error: " + err);
+        }
         var data = JSON.parse(body);
         var logData = `Title: ${data.Title}\nYear: ${data.Year}\nIMDB Rating: ${data.imdbRating}\nRotten Tomatoes Rating: ${data.ratings}\nCountry: ${data.Country}\nLanguage: ${data.Language}\nPlot: ${data.Plot}\nActors: ${data.Actors}\n-----------`;
         console.log(logData);
@@ -24,7 +27,7 @@ function findSong(song) {
     var spotify = new Spotify(keys.spotify);
     spotify.search({ type: "track", query: song, limit: 1}, function(err, data) {
         if (err) {
-        return console.log("Error: " + err);
+            return console.log("Error: " + err);
         }
         var info = data.tracks.items[0];
         var logData = `Artist(s): ${info.artists[0].name}\nSong Title: ${info.name}\nPreview Spotify Link: ${info.preview_url}\nAlbum: ${info.album.name}\n------------`;
@@ -37,7 +40,7 @@ function getTweets() {
     var params = {screen_name: "susehan27", limit: 20};
     client.get("statuses/user_timeline", params, function(err, tweets, response) {
         if (err) {
-        return console.log("Error: " + err);
+            return console.log("Error: " + err);
         }
         for (var i=0; i < tweets.length; i ++) {
             var tweets = `Tweet #(${i+1})\nTweet: ${tweets[i].text}\n`
@@ -47,19 +50,39 @@ function getTweets() {
       });
 }
 
-if (searchType === "movie-this") {
-    console.log("Searching for movie...");
-    findMovie(searchTerm);
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function(err, data) {
+        if (err) {
+            return console.log("Error: " + err);
+        }
+        var split = data.split(",");
+        searchType = split[0];
+        searchTerm = split[1];
+        runApp();
+    })
 }
-else if (searchType === "spotify-this-song") {
-    console.log("Searching for song...");
-    findSong(searchTerm);
 
+function runApp() {
+    if (searchType === "movie-this") {
+        console.log("Searching for movie...");
+        findMovie(searchTerm);
+    }
+    else if (searchType === "spotify-this-song") {
+        console.log("Searching for song...");
+        findSong(searchTerm);
+    
+    }
+    else if (searchType === "my-tweets") {
+        console.log("Retrieving latest 20 tweets...");
+        getTweets();
+    }
+    else if (searchType === "do-what-it-says") {
+        console.log("Reading random.txt...");
+        doWhatItSays();
+    }
+    else {
+        console.log("error");
+    } 
 }
-else if (searchType === "my-tweets") {
-    console.log("Retrieving latest 20 tweets...");
-    getTweets();
-}
-else {
-    console.log("error");
-}
+
+runApp();
